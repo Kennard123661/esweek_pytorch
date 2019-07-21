7a110d4f7029c530873021b63b0b9bd5a3a3f282
Kennard Ng <kennard_ng@outlook.com>
2019-07-21 17:32:48 +0800

    add feature model

---
+++ models/feature_model.py
@@ -0,0 +1,41 @@
+import os
+import sys
+
+import torch.nn as nn
+from data.feature_data import NUM_FEATURES
+
+if __name__ == '__main__':
+    base_dir = os.path.abspath(os.path.join(
+        os.path.dirname(__file__), '..'))
+    sys.path.append(base_dir)
+
+
+class FeatureModel(nn.Module):
+    def __init__(self):
+        super(FeatureModel, self).__init__()
+        self.layers = [
+            nn.Linear(in_features=NUM_FEATURES, out_features=4),
+            nn.ReLU(inplace=True),
+            nn.Linear(in_features=4, out_features=8),
+            nn.ReLU(inplace=True),
+            nn.Linear(in_features=8, out_features=8)]
+        self.init_weights()
+
+    def init_weights(self):
+        for layer in self.layers:
+            if isinstance(layer, nn.Linear):
+                nn.init.xavier_normal_(layer.weight)
+                nn.init.constant_(layer.bias, 0.0)
+
+    def forward(self, x):
+        outputs = list()
+        output = x
+        for layer in self.layers:
+            output = layer(output)
+            if isinstance(layer, nn.Linear):
+                outputs.append(output)
+        return outputs
+
+
+if __name__ == '__main__':
+    raise NotImplementedError
